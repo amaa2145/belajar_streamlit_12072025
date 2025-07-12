ed52198b96782b629ca17d6618783c731d07509b
ade irma aprilia.s <adeirmaapriliasafitri@gmail.com>
2025-07-12 08:48:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 
 st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
 
-st.markdown("*Streamlit* is **really** ***cool***.")
+st.markdown("*Streamlit* is **really** ***cool***."🥹)
