43382bf84047a8b3d680301a383328462589acf1
ade irma aprilia.s <adeirmaapriliasafitri@gmail.com>
2025-07-12 09:00:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,5 @@ st.title("_Streamlit_ is :blue[cool] :sunglasses:")
 st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
 
 st.markdown("*Streamlit* is **really** ***cool***.")
+
+st.metric(label="Temperature", value="70 °F", delta="1.2 °F")
