73c35723c8bb6d8020d3aa316f7904ad8cc609d0
ade irma aprilia.s <adeirmaapriliasafitri@gmail.com>
2025-07-12 09:04:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ col1.metric("Temperature", "70 °F", "1.2 °F")
 col2.metric("Wind", "9 mph", "-8%")
 col3.metric("Humidity", "86%", "4%")
 
+import streamlit as st
+import pandas as pd
+import numpy as np
+
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.line_chart(chart_data)
