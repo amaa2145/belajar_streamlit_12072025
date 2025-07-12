90dd165a3517c7564edcb2b141322ffa608b8041
ade irma aprilia.s <adeirmaapriliasafitri@gmail.com>
2025-07-12 08:44:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
 st.write("Hello, *World!* :sunglasses:")
+
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+
+st.subheader("_Streamlit_ is :blue[cool] :sunglasses:")
+
+st.markdown("*Streamlit* is **really** ***cool***.")
