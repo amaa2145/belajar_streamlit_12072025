afa50bea1bc5e00cebf6730a83582e6d782b412d
ade irma aprilia.s <adeirmaapriliasafitri@gmail.com>
2025-07-12 09:20:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.image("sunrise.jpg", caption="Sunrise by the mountains")
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
 
 st.line_chart(chart_data)
