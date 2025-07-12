07ade5f300a14314fc320a9c39dce8f0ecb88879
ade irma aprilia.s <adeirmaapriliasafitri@gmail.com>
2025-07-12 09:20:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,3 +33,17 @@ if st.button("Aloha", type="tertiary"):
 
 if st.button("Say hello"):
     st.write("Why hello there")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
